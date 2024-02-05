478d928915ceddd7490fd12173b34806f1c61284
Raphael Laurentino da Silva <77465404+RaphaelTW@users.noreply.github.com>
2024-02-05 14:53:23 -0300

    style

--- DetalhesScreen.js
+++ DetalhesScreen.js
@@ -3,81 +3,85 @@ import { View, Text, Button, StyleSheet, Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const DetalhesScreen = ({ route, navigation }) => {
-  const { cadastro } = route.params;
+    const { cadastro } = route.params;
 
-  const handleEditar = async () => {
-    // Implementar a lógica para edição
-    // Pode ser abrir uma tela de edição ou modais, dependendo da sua preferência
-    Alert.alert('Editar Cadastro', 'Lógica de edição ainda não implementada.');
-  };
+    const handleEditar = async () => {
+        // Implementar a lógica para edição
+        // Pode ser abrir uma tela de edição ou modais, dependendo da sua preferência
+        Alert.alert('Editar Cadastro', 'Lógica de edição ainda não implementada.');
+    };
 
-const handleExcluir = async () => {
-  try {
-    // Remover o cadastro do AsyncStorage
-    await AsyncStorage.removeItem(cadastro.nomeSite);
-    // Atualizar a lista de cadastros removendo o cadastro excluído
-    setCadastros(prevCadastros => prevCadastros.filter(item => item.nomeSite !== cadastro.nomeSite));
-    // Alertar o usuário que o cadastro foi excluído
-    Alert.alert('Cadastro Excluído', 'O cadastro foi excluído com sucesso.');
-  } catch (error) {
-    console.error('Erro ao excluir cadastro:', error);
-    // Alertar o usuário em caso de erro
-    Alert.alert('Erro', 'Ocorreu um erro ao excluir o cadastro. Por favor, tente novamente.');
-  }
-};
+    const handleExcluir = async () => {
+        try {
+            // Remover o cadastro do AsyncStorage
+            await AsyncStorage.removeItem(cadastro.nomeSite);
+            // Atualizar a lista de cadastros removendo o cadastro excluído
+            setCadastros(prevCadastros => prevCadastros.filter(item => item.nomeSite !== cadastro.nomeSite));
+            // Alertar o usuário que o cadastro foi excluído
+            Alert.alert('Cadastro Excluído', 'O cadastro foi excluído com sucesso.');
+        } catch (error) {
+            console.error('Erro ao excluir cadastro:', error);
+            // Alertar o usuário em caso de erro
+            Alert.alert('Erro', 'Ocorreu um erro ao excluir o cadastro. Por favor, tente novamente.');
+        }
+    };
 
 
-  const handleVoltar = () => {
-    navigation.goBack();
-  };
+    const handleVoltar = () => {
+        navigation.goBack();
+    };
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Detalhes do Cadastro</Text>
-      <View style={styles.card}>
-        <Text style={styles.cardText}>Nome do Site: {cadastro.nomeSite}</Text>
-        <Text style={styles.cardText}>E-mail ou Username: {cadastro.emailUsername}</Text>
-        <Text style={styles.cardText}>Senha: {cadastro.senha}</Text>
-      </View>
-      <View style={styles.buttonContainer}>
-        <Button title="Editar" onPress={handleEditar} color="#008000" />
-        <Button title="Excluir" onPress={handleExcluir} color="#FF0000" />
-        <Button title="Voltar" onPress={handleVoltar} />
-      </View>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Detalhes do Cadastro</Text>
+            <View style={styles.card}>
+                <Text style={styles.cardText}>Nome do Site: {cadastro.nomeSite}</Text>
+                <Text style={styles.cardText}>E-mail ou Username: {cadastro.emailUsername}</Text>
+                <Text style={styles.cardText}>Senha: {cadastro.senha}</Text>
+            </View>
+            <View style={styles.buttonContainer}>
+                <Button style={styles.btn} title="Editar" onPress={handleEditar} color="#008000" />
+                <Button style={styles.btn} title="Excluir" onPress={handleExcluir} color="#FF0000" />
+                <Button style={styles.btn} title="Voltar" onPress={handleVoltar} color="#0084ff" />
+            </View>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#FFFFFF',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-  card: {
-    width: '80%',
-    backgroundColor: '#1E90FF',
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    marginBottom: 20,
-    borderRadius: 5,
-  },
-  cardText: {
-    color: '#FFFFFF',
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    width: '80%',
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#1d0053',
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+        color: '#FFFFFF',
+    },
+    card: {
+        width: '80%',
+        backgroundColor: '#ff4107',
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        marginBottom: 20,
+        borderRadius: 10,
+    },
+    cardText: {
+        color: '#FFFFFF',
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        width: '80%',
+    },
+    btn: {
+        borderRadius: 10,
+    }
 });
 
 export default DetalhesScreen;
--- HomeScreen.js
+++ HomeScreen.js
@@ -2,57 +2,57 @@ import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 const HomeScreen = ({ navigation }) => {
-  const handleCadastroPress = () => {
-    navigation.navigate('Cadastro');
-  };
+    const handleCadastroPress = () => {
+        navigation.navigate('Cadastro');
+    };
 
-  const handleListarPress = () => {
-    navigation.navigate('Lista');
-  };
+    const handleListarPress = () => {
+        navigation.navigate('Lista');
+    };
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Guardião Cego</Text>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button} onPress={handleCadastroPress}>
-          <Text style={styles.buttonText}>Cadastrar</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={handleListarPress}>
-          <Text style={styles.buttonText}>Listar</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>🛡️Cyclops Secret Keeper🛡️</Text>
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity style={styles.button} onPress={handleCadastroPress}>
+                    <Text style={styles.buttonText}>Cadastrar</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button} onPress={handleListarPress}>
+                    <Text style={styles.buttonText}>Listar</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#0F0F0F',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#FFFFFF',
-    marginBottom: 30,
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-  },
-  button: {
-    backgroundColor: '#1E90FF',
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    marginHorizontal: 10,
-    borderRadius: 5,
-  },
-  buttonText: {
-    color: '#FFFFFF',
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#1d0053',
+    },
+    title: {
+        fontSize: 19,
+        fontWeight: 'bold',
+        color: '#FFFFFF',
+        marginBottom: 30,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+    },
+    button: {
+        backgroundColor: '#ff4107',
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        marginHorizontal: 10,
+        borderRadius: 10,
+    },
+    buttonText: {
+        color: '#FFFFFF',
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
 });
 
 export default HomeScreen;
